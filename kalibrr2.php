45657ecf4fff4c385bf6136665926275a9be851d
M Naufal Fazanadi <mnf_naufal@hotmail.com>
2019-10-21 16:40:59 +0700

    Fix sound animasi

--- kalibrr2.php
+++ kalibrr2.php
@@ -1,16 +1,150 @@
 <?php
 
 function find($row, $col, $arr, $word) {
-    $count = 0;
+    $result = 0;
+
     for ($i = 0; $i < $row; $i++) {
         for ($j = 0; $j < $col; $j++) {
-            if ($arr[$i][$j] == $word[0]) {
-                for ($k = $i)
-                $count++;
+            $countWord = 0;
+            if ($arr[$i][$j] == $word[$countWord]) {
+                // cek kanan
+                $count = $j;
+                $status = true;
+                while ($countWord < strlen($word) && $count < $col && $status == true) {
+                    if ($arr[$i][$count] == $word[$countWord]) {
+                        if ($countWord == strlen($word) - 1) {
+                            $result++;
+                        }
+                        $count++;
+                        $countWord++;
+                    } else {
+                        $status = false;
+                    }
+                }
+
+                // cek kiri
+                $count = $j;
+                $status = true;
+                $countWord = 0;
+                while ($countWord < strlen($word) && $count >= 0 && $status == true) {
+                    if ($arr[$i][$count] == $word[$countWord]) {
+                        if ($countWord == strlen($word) - 1) {
+                            $result++;
+                        }
+                        $count--;
+                        $countWord++;
+                    } else {
+                        $status = false;
+                    }
+                }
+
+                // cek atas
+                $count = $i;
+                $status = true;
+                $countWord = 0;
+                while ($countWord < strlen($word) && $count >= 0 && $status == true) {
+                    if ($arr[$count][$j] == $word[$countWord]) {
+                        if ($countWord == strlen($word) - 1) {
+                            $result++;
+                        }
+                        $count--;
+                        $countWord++;
+                    } else {
+                        $status = false;
+                    }
+                }
+
+                // cek bawah
+                $count = $i;
+                $status = true;
+                $countWord = 0;
+                while ($countWord < strlen($word) && $count < $row && $status == true) {
+                    if ($arr[$count][$j] == $word[$countWord]) {
+                        if ($countWord == strlen($word) - 1) {
+                            $result++;
+                        }
+                        $count++;
+                        $countWord++;
+                    } else {
+                        $status = false;
+                    }
+                }
+
+                // cek bawah kanan
+                $count = $i;
+                $count2 = $j;
+                $status = true;
+                $countWord = 0;
+                while ($countWord < strlen($word) && $count < $row && $count2 < $col && $status == true) {
+                    if ($arr[$count][$count2] == $word[$countWord]) {
+                        if ($countWord == strlen($word) - 1) {
+                            $result++;
+                        }
+                        $count++;
+                        $count2++;
+                        $countWord++;
+                    } else {
+                        $status = false;
+                    }
+                }
+
+                // cek bawah kiri
+                $count = $i;
+                $count2 = $j;
+                $status = true;
+                $countWord = 0;
+                while ($countWord < strlen($word) && $count < $row && $count2 >= 0 && $status == true) {
+                    if ($arr[$count][$count2] == $word[$countWord]) {
+                        if ($countWord == strlen($word) - 1) {
+                            $result++;
+                        }
+                        $count++;
+                        $count2--;
+                        $countWord++;
+                    } else {
+                        $status = false;
+                    }
+                }
+
+                // cek atas kiri
+                $count = $i;
+                $count2 = $j;
+                $status = true;
+                $countWord = 0;
+                while ($countWord < strlen($word) && $count >= 0 && $count2 >= 0 && $status == true) {
+                    if ($arr[$count][$count2] == $word[$countWord]) {
+                        if ($countWord == strlen($word) - 1) {
+                            $result++;
+                        }
+                        $count--;
+                        $count2--;
+                        $countWord++;
+                    } else {
+                        $status = false;
+                    }
+                }
+
+                // cek atas kanan
+                $count = $i;
+                $count2 = $j;
+                $status = true;
+                $countWord = 0;
+                while ($countWord < strlen($word) && $count >= 0 && $count2 < $col && $status == true) {
+                    if ($arr[$count][$count2] == $word[$countWord]) {
+                        if ($countWord == strlen($word) - 1) {
+                            $result++;
+                        }
+                        $count--;
+                        $count2++;
+                        $countWord++;
+                    } else {
+                        $status = false;
+                    }
+                }
             }
         }
     }
-    return $count;
+    return $result;
 }
 
 $stdin = fopen("php://stdin", "r");
